7c6cdcdd187689925d9bfb335d32f2c8dbcc3364
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-12 17:45:14 -0600

    Create the component with a list of books

---
+++ src/components/ListBooks.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    tittle: 'Book1',
+    author: 'Author1',
+    completed: '64%',
+    chapter: '17',
+  },
+  {
+    tittle: 'Book2',
+    author: 'Author2',
+    completed: '8%',
+    chapter: '3: "A lesson learned"',
+  },
+  {
+    tittle: 'Book3',
+    author: 'Author3',
+    completed: '0%',
+    chapter: 'Introduction',
+  },
+];
+
+export default books;
