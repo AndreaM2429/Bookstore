a380b908001822121ef03549936ffe2ee0705cd6
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-12 17:57:00 -0600

    Create a component with a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="d-flex navigation">
+        <ul className="d-flex">
+          <li><h1>Bookstore</h1></li>
+          <li>
+            <Link to="/">Books</Link>
+          </li>
+          <li>
+            <Link to="/authors">Authors</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
