50c635fee7d726308dc9bb65cec96aba75c39cd3
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-14 08:53:18 -0600

    Change the path to match with categories

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import BooksPage from './routers/bookPage';
-import AuthorPage from './routers/authorPage';
+import CategoriesPage from './routers/categoriesPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<BooksPage />} />
-        <Route path="/authors" element={<AuthorPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
       </Route>
     </Routes>
   );
