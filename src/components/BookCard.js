4797d564245f25fe83a97378d3c9c0e929ab5032
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-17 18:42:24 -0600

    Display the correct data and add ButtonDelete component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,26 +1,14 @@
 import PropTypes from 'prop-types';
+import ButtonDelete from './buttons/ButtonDelete';
 
 function Bookcard({ bookArr }) {
   return (
-    <section className="d-flex book-card" key={bookArr.id}>
-      <div key={bookArr.id}>
+    <section className="d-flex book-card" key={bookArr.item_id}>
+      <div key={bookArr.item_id}>
         <h4 className="book-data">{bookArr.category}</h4>
-        <h2 className="book-data">{bookArr.tittle}</h2>
+        <h2 className="book-data">{bookArr.title}</h2>
         <p className="book-data">{bookArr.author}</p>
-        <button type="button">Delete</button>
-      </div>
-      <div className="d-flex-column book-status">
-        <div>
-          <span>{bookArr.completed}</span>
-          <span>Completed</span>
-        </div>
-        <div>
-          <span>Current Chapter</span>
-          <span>
-            Chapter
-            {bookArr.chapter}
-          </span>
-        </div>
+        <ButtonDelete id={bookArr.item_id} />
       </div>
     </section>
   );
@@ -28,11 +16,9 @@ function Bookcard({ bookArr }) {
 
 Bookcard.propTypes = {
   bookArr: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    tittle: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
-    completed: PropTypes.string.isRequired,
-    chapter: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
 };
