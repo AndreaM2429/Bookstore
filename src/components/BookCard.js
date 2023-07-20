13fd0bfdb3aeb08e463633acc51060c6aeabaeb4
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-20 10:37:17 -0600

    Add the missing components wihtout new functionality

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,11 +4,31 @@ import ButtonDelete from './buttons/ButtonDelete';
 function Bookcard({ bookArr }) {
   return (
     <section className="d-flex book-card" key={bookArr.item_id}>
-      <div key={bookArr.item_id}>
-        <h4 className="book-data">{bookArr.category}</h4>
-        <h2 className="book-data">{bookArr.title}</h2>
-        <p className="book-data">{bookArr.author}</p>
+      <div className="book-info" key={bookArr.item_id}>
+        <h4 className="catego">{bookArr.category}</h4>
+        <h2 className="title">{bookArr.title}</h2>
+        <p className="author">{bookArr.author}</p>
+        <button type="button">Comments</button>
         <ButtonDelete id={bookArr.item_id} />
+        <button type="button">Edit</button>
+      </div>
+      <div className="d-flex porcentage">
+        <div className="circle" />
+        <div>
+          <span className="d-flex-column">
+            {Math.floor(Math.random() * (90 - 30 + 1) + 30)}
+            %
+          </span>
+          <span className="complete">Completed</span>
+        </div>
+      </div>
+      <div className="chap-container d-flex-column">
+        <span className="chap-title">CURRENT CHAPTER</span>
+        <span className="chap">
+          Chapter
+          {` ${Math.floor(Math.random() * (30 - 12 + 1) + 12)}`}
+        </span>
+        <button type="button">UPDATE PROGRESS</button>
       </div>
     </section>
   );
